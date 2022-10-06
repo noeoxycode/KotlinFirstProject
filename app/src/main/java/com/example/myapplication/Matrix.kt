49c537e246146cb7ed16cc9dc70a66a7291d33e5
noe <noe@marleix.com>
2022-10-06 19:48:57 +0200

    final commit

--- app/src/main/java/com/example/myapplication/Matrix.kt
+++ app/src/main/java/com/example/myapplication/Matrix.kt
@@ -3,41 +3,59 @@ package com.example.myapplication
 import kotlin.random.Random
 
 fun main(){
-    /*val matrix = Matrix(5,5);
-    val matrix2 = Matrix(5,6);*/
-    /*println("coucou");
-    println(matrix.toString());
-    matrix[2, 2] = 5;
-    println(matrix.toString());
-    println("matrix 1 is square : " + matrix.isQuare());
-    println("matrix 2 is square : " + matrix2.isQuare());*/
-    /*val square = SquareMatrix(8);
-    println(square.toString());
+/*
+    //Création d'une matrice classique impossible car classe abstraite
+    val matrix = Matrix(5,5);
+*/
+
+/*
+    //Création d'une matrice booléenne et affichage
     val booleanMatrix = BooleanMatrix(5,8);
     println(booleanMatrix.toString());
-    println(booleanMatrix.toString());*/
-    /*val intMatrix = IntMatrix(5,9);
-    println(intMatrix.toString());
-    val intMatrix = IntMatrix(5,4);
-    intMatrix[0,0] = 5;
-    intMatrix[0,1] = 5;
-    val intMatrixBis = IntMatrix(5,4);
-    intMatrixBis[0,0] = 5;
-    intMatrixBis[0,1] = 5;
-    val finalMatrix = intMatrix * intMatrixBis;
-    println(finalMatrix.mainMatrix.toString());
-    intMatrix *= intMatrixBis;
-    println(intMatrix.mainMatrix.toString());*/
-    val mneMatrix = MinesweperBoard(5,6);
-    println(mneMatrix.mainMatrix.toString());
-    println("coucou 1");
-    val intMatrix = mneMatrix.getBoardHints();
-    println("int matrix to display");
-    println(intMatrix.toString());
-    println(intMatrix.mainMatrix.toString());
-    println("coucou 2");
-    mneMatrix.toString();
+*/
 
+/*
+    //Création et controle de deux matrices pour voir si elles sont carrées
+    val matrix1 = IntMatrix(1,5);
+    println("Matrix 1 : \n $matrix1");
+    val matrix2 = BooleanMatrix(5,5);
+    println("Matrix 2 : \n $matrix2");
+    println("matrix1 is square : " + matrix1.isQuare());
+    println("matrix2 is square : " + matrix2.isQuare());
+*/
+
+/*
+    //Multiplication de matrices
+    val intMatrix = IntMatrix(5,5);
+    val intMatrixBis = IntMatrix(5,5);
+    println("Affichage de la matrice 1 : \n $intMatrix");
+    println("Affichage de la matrice 2 : \n $intMatrixBis");
+    //On assigne 5 aux cases [0][0] puis on les multiplies avec *
+    intMatrix[0,0] = 5;
+    intMatrixBis[0,0] = 5;
+    val finalMatrix = intMatrix * intMatrixBis;
+    println("Résultat du produit des deux matrices, avec 25 en [0][0] : \n" + finalMatrix.toString());
+    //On répète l'opération avec l'opérateur *= et on obtient 125 en [0][0]
+    finalMatrix *= intMatrix;
+    println("Résultat du produit des deux matrices, avec 125 en [0][0] avec l'opérateur *= : \n" + finalMatrix.toString());
+*/
+
+/*
+    //Afficher une matrix carré
+    println("Affichage d'une matrice carré");
+    val squareMatrix = SquareMatrix(5);
+    println(squareMatrix.toString());
+    squareMatrix[0,0] = 999;
+    println("On assigne la valeur 999 à la case [0][0]");
+    println(squareMatrix.toString());
+*/
+
+/*
+    //Afficher un terrain de démineur
+    val mneMatrix = MinesweperBoard(Random.nextInt(5,10),Random.nextInt(5,10));
+    println("Terrain de " + mneMatrix.row + " row et " + mneMatrix.column + " columns");
+    println(mneMatrix.getBoardHints().toString());
+*/
 }
 
 abstract class Matrix<T>(val row: Int, val column: Int) {
@@ -52,10 +70,10 @@ abstract class Matrix<T>(val row: Int, val column: Int) {
         for (i in 0 until  this.row) {
             newString+= "[";
             for (j in 0 until  this.column) {
-                if(this.mainMatrix[i][j] == null)
-                    newString += "-,";
+                if(this.mainMatrix[i][j] == -1)
+                    newString += " * ";
                 else
-                    newString+= (this.mainMatrix[i][j].toString() + ",");
+                    newString+= (" " + this.mainMatrix[i][j].toString() + " ");
             }
             newString+= "] \n";
         }
--- app/src/main/java/com/example/myapplication/MinesweperBoard.kt
+++ app/src/main/java/com/example/myapplication/MinesweperBoard.kt
@@ -3,62 +3,53 @@ package com.example.myapplication
 class MinesweperBoard(row: Int, column: Int): BooleanMatrix(row, column) {
     fun getBoardHints (): IntMatrix {
         val intMatrixBis = IntMatrix(this.row, this.column);
-        var bomb: String = "*";
-        println(this.row-1<=0);
-        println(this.column-1<=0);
         for (i in 0 until  this.row) {
             for (j in 0 until  this.column) {
                 processAction(i,j,intMatrixBis);
             }
         }
-        println(intMatrixBis.toString());
         return intMatrixBis;
     }
 
     private fun processAction(i: Int, j: Int, intMatrixBis: IntMatrix) {
         var cptBombAround = 0;
         if(this.mainMatrix[i][j] == false){
-            if(this.row - 1 >= 0){
+            if(i-1 >= 0){
                 if(this.mainMatrix[i-1][j] == true)
                     cptBombAround+=1;
-                if(this.column-1 >= 0){
+                if(j-1 >= 0){
                     if(this.mainMatrix[i-1][j-1] == true)
                         cptBombAround+=1;
                 }
-                if(this.column+1 < column){
+                if(j+1 < column){
                     if(this.mainMatrix[i-1][j+1] == true)
                         cptBombAround+=1;
                 }
             }
-            if(this.row-1 >= 0){
-                if(this.mainMatrix[i-1][j] == true)
+            if(j+1 < column){
+                if(this.mainMatrix[i][j+1] == true)
                     cptBombAround+=1;
             }
-            if(this.column+1 < column){
-                if(this.mainMatrix[i+1][j] == true)
+            if(j-1 >= 0){
+                if(this.mainMatrix[i][j-1] == true)
                     cptBombAround+=1;
             }
-            if(this.row + 1 < row){
+            if(i+1 < row){
                 if(this.mainMatrix[i+1][j] == true)
                     cptBombAround+=1;
-                if(this.column-1 >= 0){
+                if(j-1 >= 0){
                     if(this.mainMatrix[i+1][j-1] == true)
                         cptBombAround+=1;
                 }
-                if(this.column+1 < column){
+                if(j+1 < column){
                     if(this.mainMatrix[i+1][j+1] == true)
                         cptBombAround+=1;
                 }
             }
-            println()
-            println(cptBombAround);
             intMatrixBis.mainMatrix[i][j] = cptBombAround;
         }
         else{
             intMatrixBis.mainMatrix[i][j] = -1;
-            println("bomb");
         }
     }
-
-
 }
